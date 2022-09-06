27327055379542c9ff760e92ca8fe14fcaa16dae
purevbat12 <97670057+purevbat12@users.noreply.github.com>
2022-09-06 23:50:15 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+//document.write("i bet You might have been wondering what this page's background image. but some people know this logo or symbol whatever you wanna call it. This is aperture science logo from Portal game")
+var x = document.write ();
+function handleClick(element){
+    alert( 'So i tested the javascript on this button thingy and i am suprised that it worked it was really confusing to at first and second attempts but i finally figurd it out. Jesus this javascript is so confusing to me. I hope i can learn it.' );
+    element.style="background-color:blue"
+}
